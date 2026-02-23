5d0c1bab35750d740196c4dedd82ba5fc921b606
miandry <miandrilala9@yahoo.fr>
2026-02-23 09:55:08 +0300

    save

--- MGATrans/src/services/api.ts
+++ MGATrans/src/services/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://dev.local:8888';
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === 'dev.local';
+export const API_BASE_URL = isLocal ? 'http://dev.local:8888' : 'https://mga.mizara.io';
